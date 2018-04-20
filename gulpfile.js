76afc4aad3162ded89aa6f5d2a45b77383ae46b6
LeFevre <matthewlefevre@byui.edu>
2018-04-20 11:55:38 -0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let sourcemaps = require('gulp-sourcemaps');
+let autoprefixer = require('gulp-autoprefixer');
+let browserSync = require('browsersync').create();
+
+
+
